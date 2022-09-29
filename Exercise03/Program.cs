d387bab296ad6f87adbda089896c12ded6a3f3b2
Aivenest <aivenest@gmail.com>
2022-09-29 14:36:06 +0500

    add ex03

---
+++ Exercise03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+int ImputNumber()
+{
+    Console.WriteLine("Введите число:");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetDigCount(int Number)
+{
+    // string Numbers()
+    // {
+    //     string numbers = Convert.ToString(num);
+    //     return numbers;
+    // }
+    int DigCount = 0;
+    // Number = Math.Abs(Number / 10);
+    while (Number > 0)
+    {
+        Number = Number / 10;
+        DigCount++;
+    }
+    return DigCount;
+}
+
+Console.WriteLine("Количество цифр в числе = " + GetDigCount(ImputNumber()));
