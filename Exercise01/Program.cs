b6740ec73d821fb03eb291211b720e964b51c30c
Aivenest <aivenest@gmail.com>
2022-09-29 12:49:08 +0500

    Ex01 add

---
+++ Exercise01/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetNumberThreeDigit()
+{
+
+    
+    int num = 0;
+    while (num < 100 || num > 999)
+    {
+        Console.WriteLine("Введите трехзначное число:");
+       num = Convert.ToInt32(Console.ReadLine());
+    }
+    return num;
+}
+
+int a = GetNumberThreeDigit();
+
+Console.WriteLine(a);
